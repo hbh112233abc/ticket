681559d4e65e17e85fe4a7cf58f5204c124a2acf
hbh112233abc <hbh112233abc@163.com>
2020-02-13 16:35:11 +0800

    模板修复

--- app/home/middleware/WxAuth.php
+++ app/home/middleware/WxAuth.php
@@ -19,14 +19,12 @@ class WxAuth
     {
         $app = Factory::officialAccount(config('wx'));
         $oauth = $app->oauth;
+        session('target_url', $request->url(true));
         if (empty(session('wechat_user'))) {
-            // 未登录
-            session('target_url', $request->url(true));
-
             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
             return $oauth->redirect()->send();
         }
-        $request->jssdk = $app->jssdk->buildConfig(['updateAppMessageShareData', 'updateTimelineShareData'], env('APP_DEBUG', true));
+        $request->jssdk = $app->jssdk->buildConfig(['updateAppMessageShareData', 'updateTimelineShareData'], true);
         return $next($request);
     }
 }
